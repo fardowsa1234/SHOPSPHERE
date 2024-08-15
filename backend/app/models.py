bd099b73ba99c0b4353c0b41c7755262bbbfbe0f
Jan-Muhachi <162049540+Jan-Muhachi@users.noreply.github.com>
2024-08-15 09:49:54 +0300

    Add files via upload

---
+++ backend/app/models.py
@@ -0,0 +1,30 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    role = db.Column(db.String(10), nullable=False, default='user')  # 'user' or 'admin'
+    cart = db.relationship('Cart', backref='owner', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    items = db.relationship('CartItem', backref='cart', lazy=True)
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
